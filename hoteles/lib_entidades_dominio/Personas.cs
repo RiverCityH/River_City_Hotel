b62f33297ced229fb968ca8f21bd91504128b99e
jrios <jrios@DESKTOP-L5MB1PJ>
2024-09-08 21:37:36 -0500

    Modelo Personas

---
+++ hoteles/lib_entidades_dominio/Personas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_entidades_dominio
+{
+    public class Personas
+    {
+        public class Productos
+        {
+            [Key] public virtual int Id { get; set; }
+            public virtual int TipoDocumento { get; set; }
+            public virtual string? Documento { get; set; }
+            public virtual string? Nombre { get; set; }
+            public virtual DateTime FechaNacimiento { get; set; }
+            public virtual string? Celular { get; set; }
+            public virtual int Genero { get; set; }
+            public virtual string? Direccion { get; set; }
+            public virtual string? Email { get; set; }
+            public virtual string? contraseña { get; set; }
+            public virtual bool Confirmar { get; set; }
+            public virtual bool Restablecer { get; set; }
+            public virtual string? Token { get; set; }
+            public virtual int Ciudad { get; set; }
+            public virtual bool Activo { get; set; }
+
+            [ForeignKey("Ciudad")] public virtual Ciudades? _Ciudad { get; set; }
+            [ForeignKey("TipoDocumento")] public virtual Tipos? _TipoDocumento { get; set; }
+            [ForeignKey("Genero")] public virtual Tipos? _Genero { get; set; }
+
+
+            //[NotMapped] public virtual ICollection<Productos>? Productos { get; set; }
+
+
+        }
+    }
+}

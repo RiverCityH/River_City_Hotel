8aa44c1b1c27ef5bee5141342b20953698b7f9cc
Andres Gaviria <Andres.Gaviria@sleepgroupsolutions.com>
2024-09-08 23:53:41 -0500

    Arreglo del Asp net core

---
+++ hoteles/lib_utilidades/LogHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace lib_utilidades
+{
+    public class LogHelper
+    {
+        public static ILogHelper? ILogHelper = null;
+
+        public static void Log(Exception exception, bool subError = false, [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
+        {
+            if (ILogHelper == null)
+                return;
+
+            ILogHelper.Log(exception, subError, caller, file);
+        }
+    }
+
+    public interface ILogHelper
+    {
+        void Log(Exception exception, bool subError = false, [CallerMemberName] string caller = "", [CallerFilePath] string file = "");
+    }
+}

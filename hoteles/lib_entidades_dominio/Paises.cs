27b361f0c73ce040213063986454b702cba1d192
jrios <jrios@DESKTOP-L5MB1PJ>
2024-09-08 20:09:31 -0500

    Clase Tipos

--- hoteles/lib_entidades_dominio/Paises.cs
+++ hoteles/lib_entidades_dominio/Paises.cs
@@ -1,7 +1,13 @@
-﻿namespace lib_entidades_dominio
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace lib_entidades_dominio
 {
     public class Paises
     {
+        [Key] public virtual int Id { get; set; }
+        public virtual string? Nombre { get; set; }
 
+        //[NotMapped] public virtual ICollection<Facturas>? Facturas { get; set; }
     }
 }
---
+++ hoteles/lib_entidades_dominio/Tipos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_entidades_dominio
+{
+    internal class Tipos
+    {
+        [Key] public virtual int Id { get; set; }
+        public virtual string? Nombre { get; set; }
+        public virtual int Tabla { get; set; }
+        public virtual int Accion { get; set; }
+
+        //[NotMapped] public virtual ICollection<Facturas>? Facturas { get; set; }
+    }
+}
+
